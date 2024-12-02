3d60fed35f0d88fd983b5493773977e91b37b1c3
muhamma-waqar <waqar.netdev@gmail.com>
2024-12-02 14:13:42 +0500

    zx

--- src/DomeCalandar/src/model/EventQuery.ts
+++
@@ -1,9 +0,0 @@
-export class EventQuery{
-    public pageNumber! : number;
-    public pageSize!: number;
-
-    constructor(pageNumber: number, pageSize: number){
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-    }
-}
